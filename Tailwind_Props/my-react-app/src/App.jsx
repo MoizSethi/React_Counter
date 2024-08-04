4abfabd83284d2a45ab6c53ef45309603fed9459
MoizSethi <m.moizsethi@gmail.com>
2024-08-05 00:16:06 +0500

    Tailwind props

--- Tailwind_Props/my-react-app/src/App.jsx
+++ Tailwind_Props/my-react-app/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Cards from './components/Cards';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  let myObj = {
+    username: 'Moiz_Sethi',
+    age: 24
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='bg-green-400 p-4 rounded-xl mb-4'>Tailwind And Props in React</h1>
+      <Cards username="Moiz Sethi" age="24" obj={myObj} />
+      <Cards username="Atif" age="36"/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
