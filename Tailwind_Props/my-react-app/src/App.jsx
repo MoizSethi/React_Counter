81114140414be76fc78cb6f647f30aff79e82d62
MoizSethi <m.moizsethi@gmail.com>
2024-08-05 00:19:19 +0500

    Url Updated

--- Tailwind_Props/my-react-app/src/App.jsx
+++ Tailwind_Props/my-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import Cards from './components/Cards';
+import Cards from '/components/Cars.jsx';
 
 function App() {
   const [count, setCount] = useState(0);
